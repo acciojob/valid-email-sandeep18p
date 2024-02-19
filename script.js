a96dc215f3077af6c819d31b250d93eaf827cb37
Sandeep Pansari <86551476+sandeep18p@users.noreply.github.com>
2024-02-19 21:38:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 function validEmail(str) {
-  //your JS code here.
+// Regular expression for validating email addresses
+  const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  
+  // Test the input string against the regex pattern
+  return emailRegex.test(str);
 }
 
 // Do not change the code below.
